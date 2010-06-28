267aa9363a885caa6689797acf34ad7fc71fb966
Peteris Krumins <peteris.krumins@gmail.com>
2010-06-28 05:15:19 +0300

    this node.js program converts rgba terminals to a video by using node-video library

---
+++ tests/full-frames/tovideo.js
@@ -0,0 +1,27 @@
+var VideoLib = require('video');
+var Buffer = require('buffer').Buffer;
+var fs = require('fs');
+var sys = require('sys');
+
+var fileRx = new RegExp(/^terminal-(\d+).rgba/);
+
+var files = fs.readdirSync('.').sort().filter(
+    function (f) { return fileRx.test(f) }
+);
+
+function baseName(fileName) {
+    return fileName.slice(0, fileName.indexOf('.'));
+}
+
+var fixedVideo = new VideoLib.FixedVideo(720, 400, 32); // width, height, quality
+fixedVideo.setOutputFile('video.ogv');
+
+var buf = new Buffer(1152000);
+files.forEach(function (file) {
+    var terminal = fs.readFileSync(file, 'binary');
+    buf.write(terminal, 'binary');
+    fixedVideo.newFrame(buf);
+});
+
+fixedVideo.end();
+
