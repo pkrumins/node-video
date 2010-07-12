a9edd417eeebc7cccd94bb88a0788da4a30b851e
Peteris Krumins <peteris.krumins@gmail.com>
2010-07-12 09:08:12 +0300

    fixed full-frame tests by converting all rgba buffers to rgb

--- tests/full-frames/tovideo.js
+++ tests/full-frames/tovideo.js
@@ -3,7 +3,7 @@ var Buffer = require('buffer').Buffer;
 var fs = require('fs');
 var sys = require('sys');
 
-var fileRx = new RegExp(/^terminal-(\d+).rgba/);
+var fileRx = new RegExp(/^terminal-(\d+).rgb$/);
 
 var files = fs.readdirSync('.').sort().filter(
     function (f) { return fileRx.test(f) }
@@ -18,6 +18,7 @@ fixedVideo.setOutputFile('video.ogv');
 
 var buf = new Buffer(1152000);
 files.forEach(function (file) {
+    sys.log(file);
     var terminal = fs.readFileSync(file, 'binary');
     buf.write(terminal, 'binary');
     fixedVideo.newFrame(buf);
